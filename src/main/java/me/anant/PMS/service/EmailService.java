d68358a995dead6ab6eab68cf6391bb6821167fd
Chinmay Chaudhari <cchaudhari5@gmail.com>
2020-10-03 15:56:33 +0530

    Removed EMailConfig File and Removed COmments from EmailService

--- src/main/java/me/anant/PMS/service/EmailService.java
+++ src/main/java/me/anant/PMS/service/EmailService.java
@@ -10,40 +10,30 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
-	
-	//Added Autowired For Java MAil Sender
+
 	@Autowired
-    private JavaMailSender javaMailSender;
-	
+	private JavaMailSender javaMailSender;
+
 	public void send(String to, String subject, String body) {
-		String template = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\r\n" + 
-				"<html>\r\n" + 
-				" <head>\r\n" + 
-				"<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />"
-				+ "<style>"
+		String template = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\r\n"
+				+ "<html>\r\n" + " <head>\r\n"
+				+ "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />" + "<style>"
 				+ "table {width: 100%;border-collapse: collapse;}"
-				+ "table, th, td {border: 1px solid black; padding: 5px}"
-				+ "</style>"
-				+ "</head>"
-				+ "<body>"
-				+ body
-				+ "<br><br>"
-				+ "Thanks & Regards,<br>"
-				+ "Team PMS"
-				+ "</body>"
-				+ "</html>";
-		
-		 MimeMessagePreparator preparator = new MimeMessagePreparator() {
-            public void prepare(MimeMessage mimeMessage) throws Exception {
-                MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
-                message.setTo(to);
-                message.setFrom("system@gmail.com");
-                message.setSubject(subject);
-                message.setText(template, true);
-            }
-        };
-        
-        //Replaced EmailConfig with JavaMailSender
-        javaMailSender.send(preparator);
+				+ "table, th, td {border: 1px solid black; padding: 5px}" + "</style>" + "</head>" + "<body>" + body
+				+ "<br><br>" + "Thanks & Regards,<br>" + "Team PMS" + "</body>" + "</html>";
+
+		MimeMessagePreparator preparator = new MimeMessagePreparator() {
+			public void prepare(MimeMessage mimeMessage) throws Exception {
+				MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true);
+
+				message.setTo(to);
+				message.setFrom("system@gmail.com");
+				message.setSubject(subject);
+				message.setText(template, true);
+
+			}
+		};
+
+		javaMailSender.send(preparator);
 	}
 }
