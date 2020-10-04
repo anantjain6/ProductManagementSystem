35e56bb2dc0cb05ae82284bfa5696848a7a4efcd
Omkar Shivadekar <Omkar Shivadekar@omy6987>
2020-10-05 00:09:15 +0530

    #3 - handled image file in controller

--- src/main/java/me/anant/PMS/controller/ProductController.java
+++ src/main/java/me/anant/PMS/controller/ProductController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -37,15 +38,26 @@ public class ProductController {
 	}
 	
 	@PostMapping("/admin/product/add")
-	public String add(@ModelAttribute("command") @Valid Product product, BindingResult result, Model model, final RedirectAttributes redirectAttributes) {
+	public String add(@ModelAttribute("command") @Valid Product product, @RequestParam("imageFile") MultipartFile imageFile, 
+			BindingResult result, Model model, final RedirectAttributes redirectAttributes) {
 		if(result.hasErrors()) {
 			model.addAttribute("pcList", categoryService.get());
 			return "admin/product/add";
 		}
-		productService.save(product);
-		redirectAttributes.addFlashAttribute("msg", "Product added successfully");
-		redirectAttributes.addFlashAttribute("class", "alert-success");
-		return "redirect:/admin/product/add";
+		
+		if(imageFile.isEmpty()) {
+			redirectAttributes.addFlashAttribute("msg", "Please select product picture");
+			redirectAttributes.addFlashAttribute("class", "alert-danger");
+			return "redirect:/admin/product/add";
+		}else {
+			String filePath = productService.saveImage(imageFile);
+			product.setImageName(filePath);
+			productService.save(product);
+			redirectAttributes.addFlashAttribute("msg", "Product added successfully");
+			redirectAttributes.addFlashAttribute("class", "alert-success");
+			return "redirect:/admin/product/add";
+		}
+		
 	}
 	
 	@GetMapping("/admin/product/list")
