c967e93ad759d4e8d3f6a87ec660e8f7d3b2aa9b
Omkar Shivadekar <Omkar Shivadekar@omy6987>
2020-10-05 00:01:56 +0530

    added configuration for image file

---
+++ src/main/java/me/anant/PMS/config/DocumentStorageProperty.java
@@ -0,0 +1,20 @@
+package me.anant.PMS.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix="document")
+public class DocumentStorageProperty {
+	
+	private String uploadDirectory;
+
+	public String getUploadDirectory() {
+		return uploadDirectory;
+	}
+
+	public void setUploadDirectory(String uploadDirectory) {
+		this.uploadDirectory = uploadDirectory;
+	}
+
+}
