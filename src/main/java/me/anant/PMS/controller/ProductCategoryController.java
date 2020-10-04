471db5173b6e1a04144ec51711c69b300fd4b19c
Ruban-Ramesh <45206182+Ruban-Ramesh@users.noreply.github.com>
2020-10-04 15:06:12 +0530

    Added CRUD operation for Product Category in Admin Panel

--- src/main/java/me/anant/PMS/controller/ProductCategoryController.java
+++ src/main/java/me/anant/PMS/controller/ProductCategoryController.java
@@ -3,14 +3,23 @@ package me.anant.PMS.controller;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import me.anant.PMS.model.Product;
+import me.anant.PMS.model.ProductCategory;
 import me.anant.PMS.service.ProductCategoryService;
 
 @Controller
@@ -18,13 +27,68 @@ public class ProductCategoryController {
 	@Autowired
 	ProductCategoryService categoryService;
 	
+	@GetMapping("/admin/product_category/add")
+	public ModelAndView addView() {
+		ModelAndView modelAndView = new ModelAndView("admin/product_category/add");
+		modelAndView.addObject("command",new ProductCategory());
+		return modelAndView;
+	}
+
+	@PostMapping("/admin/product_category/add")
+	public String add(@ModelAttribute("command") @Valid ProductCategory productCategory, BindingResult result, Model model, final RedirectAttributes redirectAttributes){
+		if(result.hasErrors()){
+			return "admin/product_category/add";
+		}
+		categoryService.save(productCategory);
+		redirectAttributes.addFlashAttribute("msg", "Product Category added successfully");
+		redirectAttributes.addFlashAttribute("class", "alert-success");
+		return "redirect:/admin/product_category/list";
+	}
+ 
+	@GetMapping("admin/product_category/list")
+	public ModelAndView productCategoryList() {
+		ModelAndView modelAndView = new ModelAndView("admin/product_category/list");
+		modelAndView.addObject("cList", categoryService.get());
+		return modelAndView;
+	}
+
+	@GetMapping("/admin/product_category/update")
+	public ModelAndView updateView(long id) {
+		Optional<ProductCategory> optional = categoryService.findById(id);
+		ProductCategory productCategory = optional.get();
+		ModelAndView modelAndView = new ModelAndView("admin/product_category/add");
+		modelAndView.addObject("command", productCategory);
+		return modelAndView;
+	}
+	
+	@PostMapping("/admin/product_category/update")
+	public String updateView(@ModelAttribute("command") @Valid ProductCategory productCategory, BindingResult result, Model model, final RedirectAttributes redirectAttributes) {
+		if(result.hasErrors()) {
+			model.addAttribute("pcList", categoryService.get());
+			return "admin/product_category/add";
+		}
+		categoryService.save(productCategory);
+		redirectAttributes.addFlashAttribute("msg", "Product Category Updated successfully");
+		redirectAttributes.addFlashAttribute("class", "alert-success");
+		return "redirect:/admin/product_category/list";
+	}
+	
+	@GetMapping("/admin/product_category/delete")
+	public String list(@RequestParam("id") long id,
+			final RedirectAttributes redirectAttributes) {
+		categoryService.delete(id);
+		redirectAttributes.addFlashAttribute("msg", "Product Category deleted successfully");
+		redirectAttributes.addFlashAttribute("class", "alert-success");
+		return "redirect:/admin/product_category/list";
+	}
+
 	@GetMapping("admin/category/list")
 	public ModelAndView categoryList() {
 		ModelAndView modelAndView = new ModelAndView("admin/category/list");
 		modelAndView.addObject("cList", categoryService.get());
 		return modelAndView;
 	}
-	
+
 	@GetMapping("admin/category/report")
 	public ModelAndView report(@RequestParam("id") long id) {
 		Set<Product> pSet = categoryService.findById(id).get().getProducts();
--- src/main/webapp/includes/header.jsp
+++ src/main/webapp/includes/header.jsp
@@ -23,7 +23,8 @@
         	<ul class="navbar-nav mr-auto">
 	            <sec:authorize access="hasRole('ADMIN')">
                 	<li class="nav-item"><a class="nav-link" href="/admin/product/list">Product</a></li>
-                	<li class="nav-item"><a class="nav-link" href="/admin/order/list">Order</a></li>
+					<li class="nav-item"><a class="nav-link" href="/admin/order/list">Order</a></li>
+					<li class="nav-item"><a class="nav-link" href="/admin/product_category/list">Category</a></li>
                 	<li class="nav-item"><a class="nav-link" href="/admin/product/report">Product Report</a></li>
                 	<li class="nav-item"><a class="nav-link" href="/admin/category/list">Category Report</a></li>
                 </sec:authorize>
