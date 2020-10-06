8125557ff2a6ab3bf21a50e747533b52726455db
Ram G Suri <ramgsuri@rediffmail.com>
2020-10-07 00:11:35 +0530

    [Ram] Modifies to add javadoc comments  (#19)
    * Added javadoc comments
    
    Co-authored-by: ramgsuri <ramgsuri@gmail.com>

--- src/main/java/me/anant/PMS/controller/ProductController.java
+++ src/main/java/me/anant/PMS/controller/ProductController.java
@@ -20,6 +20,9 @@ import me.anant.PMS.model.Product;
 import me.anant.PMS.service.ProductCategoryService;
 import me.anant.PMS.service.ProductService;
 
+/**
+ * This Controller is responsible for CRUD operations related to Products.
+ */
 @Controller
 public class ProductController {
 	@Autowired
@@ -27,7 +30,11 @@ public class ProductController {
 	
 	@Autowired
 	ProductCategoryService categoryService;
-	
+
+	/**
+	 * This api is responsible to fetch the model and view for add Product.
+	 * @return ModelAndView
+	 */
 	@GetMapping("/admin/product/add")
 	public ModelAndView addView() {
 		ModelAndView modelAndView = new ModelAndView("admin/product/add");
@@ -35,7 +42,15 @@ public class ProductController {
 		modelAndView.addObject("command", new Product());
 		return modelAndView;
 	}
-	
+
+	/**
+	 * This api is responsible to add Product.
+	 * @param product
+	 * @param result
+	 * @param model
+	 * @param redirectAttributes
+	 * @return String redirect path
+	 */
 	@PostMapping("/admin/product/add")
 	public String add(@ModelAttribute("command") @Valid Product product, BindingResult result, Model model, final RedirectAttributes redirectAttributes) {
 		if(result.hasErrors()) {
@@ -47,7 +62,11 @@ public class ProductController {
 		redirectAttributes.addFlashAttribute("class", "alert-success");
 		return "redirect:/admin/product/add";
 	}
-	
+
+	/**
+	 * This Get api is responsible to view Product List
+	 * @return ModelAndView
+	 */
 	@GetMapping("/admin/product/list")
 	public ModelAndView list() {
 		List<Product> pList = productService.get();
@@ -55,7 +74,13 @@ public class ProductController {
 		modelAndView.addObject("pList", pList);
 		return modelAndView;
 	}
-	
+
+	/**
+	 * This GET api is responsible to delete the product.
+	 * @param id
+	 * @param redirectAttributes
+	 * @return redirect path
+	 */
 	@GetMapping("/admin/product/delete")
 	public String list(@RequestParam("id") long id,
 			final RedirectAttributes redirectAttributes) {
@@ -64,7 +89,12 @@ public class ProductController {
 		redirectAttributes.addFlashAttribute("class", "alert-success");
 		return "redirect:/admin/product/list";
 	}
-	
+
+	/**
+	 * This api is responsible to fetch the model and view for update Products Screen.
+	 * @param id
+	 * @return ModelAndView
+	 */
 	@GetMapping("/admin/product/update")
 	public ModelAndView updateView(long id) throws ProductNotFoundException {
 		Optional<Product> optional = productService.findById(id);
@@ -74,7 +104,15 @@ public class ProductController {
 		modelAndView.addObject("pcList", categoryService.get());
 		return modelAndView;
 	}
-	
+
+	/**
+	 * This POST api is responsible to update the Product.
+	 * @param product
+	 * @param result
+	 * @param model
+	 * @param redirectAttributes
+	 * @return String redirect path
+	 */
 	@PostMapping("/admin/product/update")
 	public String updateView(@ModelAttribute("command") @Valid Product product, BindingResult result, Model model, final RedirectAttributes redirectAttributes) {
 		if(result.hasErrors()) {
@@ -86,7 +124,11 @@ public class ProductController {
 		redirectAttributes.addFlashAttribute("class", "alert-success");
 		return "redirect:/admin/product/list";
 	}
-	
+
+	/**
+	 * This api is responsible to generate report related to Product.
+	 * @return ModelAndView
+	 */
 	@GetMapping("/admin/product/report")
 	public ModelAndView report() {
 		ModelAndView modelAndView = new ModelAndView("admin/product/report");
